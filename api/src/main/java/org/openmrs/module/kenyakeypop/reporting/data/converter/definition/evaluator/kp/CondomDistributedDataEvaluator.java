41ec20717e196ed3272eb625e1f0c96884a86d70
Stephen Chege <njorocs@gmail.com>
2020-09-22 11:07:06 +0300

    Changed computation of Commodities distributed per month in the KP cohort register. Given by sum total of clinical visit, sti treatment and peer calendar

--- api/src/main/java/org/openmrs/module/kenyakeypop/reporting/data/converter/definition/evaluator/kp/CondomDistributedDataEvaluator.java
+++ api/src/main/java/org/openmrs/module/kenyakeypop/reporting/data/converter/definition/evaluator/kp/CondomDistributedDataEvaluator.java
@@ -11,7 +11,6 @@ package org.openmrs.module.kenyakeypop.reporting.data.converter.definition.evalu
 
 import org.openmrs.annotation.Handler;
 import org.openmrs.module.kenyakeypop.reporting.data.converter.definition.kp.CondomDistributedDataDefinition;
-import org.openmrs.module.kenyakeypop.reporting.data.converter.definition.kp.CondomRequirementsDataDefinition;
 import org.openmrs.module.reporting.data.person.EvaluatedPersonData;
 import org.openmrs.module.reporting.data.person.definition.PersonDataDefinition;
 import org.openmrs.module.reporting.data.person.evaluator.PersonDataEvaluator;
@@ -21,10 +20,11 @@ import org.openmrs.module.reporting.evaluation.querybuilder.SqlQueryBuilder;
 import org.openmrs.module.reporting.evaluation.service.EvaluationService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
 
-/**
+/**api
  * Evaluates a PersonDataDefinition
  */
 @Handler(supports = CondomDistributedDataDefinition.class, order = 50)
@@ -33,12 +33,29 @@ public class CondomDistributedDataEvaluator implements PersonDataEvaluator {
 	@Autowired
 	private EvaluationService evaluationService;
 	
+	SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+	
 	public EvaluatedPersonData evaluate(PersonDataDefinition definition, EvaluationContext context)
 	        throws EvaluationException {
 		EvaluatedPersonData c = new EvaluatedPersonData(definition, context);
+		String reportDate = DATE_FORMAT.format(context.getEvaluationDate());
 		
-		String qry = "select c.client_id as client_id ,coalesce(c.monthly_male_condoms_distributed,0) + coalesce(c.monthly_female_condoms_distributed,0) as monthly_condoms_distributed from kenyaemr_etl.etl_peer_calendar c\n"
-		        + "group by c.client_id;";
+		String qry = "select c.client_id, sum(ifnull(v1.v_condoms_dist,0)+ifnull(p1.pc_condoms_dist,0)+ifnull(t1.t_condoms_dist,0)) as condoms_distributed from kenyaemr_etl.etl_contact c\n"
+		        + "left join (select t.client_id,sum(ifnull(t.no_of_condoms,0)) as t_condoms_dist from kenyaemr_etl.etl_sti_treatment t where t.visit_date between date_sub(\""
+		        + reportDate
+		        + "\", INTERVAL 30 DAY) and \""
+		        + reportDate
+		        + "\" group by t.client_id )t1 on c.client_id = t1.client_id\n"
+		        + "left join (select v.client_id,sum((ifnull(v.female_condoms_no,0)+ifnull(v.male_condoms_no,0))) as v_condoms_dist from kenyaemr_etl.etl_clinical_visit v where v.visit_date between date_sub(\""
+		        + reportDate
+		        + "\", INTERVAL 30 DAY) and \""
+		        + reportDate
+		        + "\" group by v.client_id)v1 on c.client_id = v1.client_id\n"
+		        + "left join (select p.client_id, p.monthly_male_condoms_distributed,p.monthly_female_condoms_distributed,sum(ifnull(p.monthly_male_condoms_distributed,0)+ifnull(p.monthly_female_condoms_distributed,0)) as pc_condoms_dist from kenyaemr_etl.etl_peer_calendar p where p.visit_date between date_sub(\""
+		        + reportDate
+		        + "\", INTERVAL 30 DAY) and \""
+		        + reportDate
+		        + "\" group by p.client_id)p1 on c.client_id = p1.client_id\n" + "group by c.client_id;";
 		
 		SqlQueryBuilder queryBuilder = new SqlQueryBuilder();
 		Date startDate = (Date) context.getParameterValue("startDate");
--- api/src/main/java/org/openmrs/module/kenyakeypop/reporting/data/converter/definition/evaluator/kp/NeedlesAndSyringesDistributedDataEvaluator.java
+++ api/src/main/java/org/openmrs/module/kenyakeypop/reporting/data/converter/definition/evaluator/kp/NeedlesAndSyringesDistributedDataEvaluator.java
@@ -21,6 +21,7 @@ import org.openmrs.module.reporting.evaluation.querybuilder.SqlQueryBuilder;
 import org.openmrs.module.reporting.evaluation.service.EvaluationService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
 
@@ -33,11 +34,25 @@ public class NeedlesAndSyringesDistributedDataEvaluator implements PersonDataEva
 	@Autowired
 	private EvaluationService evaluationService;
 	
+	SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+	
 	public EvaluatedPersonData evaluate(PersonDataDefinition definition, EvaluationContext context)
 	        throws EvaluationException {
 		EvaluatedPersonData c = new EvaluatedPersonData(definition, context);
 		
-		String qry = "select v.client_id as client_id ,sum(v.syringes_needles_no) as needles_and_syringes_distributed from kenyaemr_etl.etl_clinical_visit v group by v.client_id;";
+		String reportDate = DATE_FORMAT.format(context.getEvaluationDate());
+		
+		String qry = "select c.client_id, sum(ifnull(v1.v_ns_dist,0)+ifnull(p1.pc_ns_dist,0)) as n_s_distributed from kenyaemr_etl.etl_contact c\n"
+		        + "left join (select v.client_id,sum((ifnull(v.syringes_needles_no,0)+ifnull(v.male_condoms_no,0))) as v_ns_dist from kenyaemr_etl.etl_clinical_visit v where v.visit_date between date_sub(\""
+		        + reportDate
+		        + "\",INTERVAL 30 DAY) and \""
+		        + reportDate
+		        + "\" group by v.client_id)v1 on c.client_id = v1.client_id\n"
+		        + "left join (select p.client_id, sum(ifnull(p.monthly_n_and_s_distributed,0)) as pc_ns_dist from kenyaemr_etl.etl_peer_calendar p where p.visit_date between date_sub(\""
+		        + reportDate
+		        + "\",INTERVAL 30 DAY) and \""
+		        + reportDate
+		        + "\" group by p.client_id)p1 on c.client_id = p1.client_id\n" + "group by c.client_id;";
 		
 		SqlQueryBuilder queryBuilder = new SqlQueryBuilder();
 		Date startDate = (Date) context.getParameterValue("startDate");
