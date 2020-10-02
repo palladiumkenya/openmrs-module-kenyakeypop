410d32b39b6996eb1f6af571691c56da37426db9
makombe <kennedymakombe@gmail.com>
2020-10-02 11:54:27 +0300

    Validate peer && peer educator names

--- api/src/main/java/org/openmrs/module/kenyakeypop/metadata/KeypopSecurityMetadata.java
+++ api/src/main/java/org/openmrs/module/kenyakeypop/metadata/KeypopSecurityMetadata.java
@@ -20,45 +20,45 @@ import static org.openmrs.module.metadatadeploy.bundle.CoreConstructors.role;
 @Component
 @Requires(org.openmrs.module.kenyaemr.metadata.SecurityMetadata.class)
 public class KeypopSecurityMetadata extends AbstractMetadataBundle {
-
+	
 	@Autowired
 	@Qualifier("userService")
 	private UserService userService;
-
+	
 	public static final class _Privilege {
-
+		
 		public static final String KP_MODULE_APP = "App: kenyaemrkeypop.home";
-
+		
 		public static final String KP_PROVIDER_APP = "App: kenyakeypop.keypopulation.provider";
-
+		
 		public static final String PEER_CALENDAR_APP = "App: kenyakeypop.keypopulation.calendar";
 	}
-
+	
 	public static final class _Role {
-
+		
 		public static final String PEER_EDUCATOR = "Peer Educator";
-
+		
 		public static final String APPLICATION_KP_MODULE = "Key Population Module";
-
+		
 	}
-
+	
 	/**
 	 * @see org.openmrs.module.metadatadeploy.bundle.AbstractMetadataBundle#install()
 	 */
 	@Override
 	public void install() {
-
+		
 		install(privilege(_Privilege.KP_MODULE_APP, "Able to access Key Population module features"));
 		install(privilege(_Privilege.KP_PROVIDER_APP, "Able to access KP Provider module features"));
 		install(privilege(_Privilege.PEER_CALENDAR_APP, "Able to access Peer Calendar features"));
-
+		
 		install(role(_Role.APPLICATION_KP_MODULE, "Can access Key Population module App",
-				idSet(org.openmrs.module.kenyaemr.metadata.SecurityMetadata._Role.API_PRIVILEGES_VIEW_AND_EDIT),
-				idSet(_Privilege.KP_PROVIDER_APP, _Privilege.KP_MODULE_APP, _Privilege.PEER_CALENDAR_APP)));
-
+		    idSet(org.openmrs.module.kenyaemr.metadata.SecurityMetadata._Role.API_PRIVILEGES_VIEW_AND_EDIT),
+		    idSet(_Privilege.KP_PROVIDER_APP, _Privilege.KP_MODULE_APP, _Privilege.PEER_CALENDAR_APP)));
+		
 		install(role(_Role.PEER_EDUCATOR, "Can access Key Population module App",
-				idSet(org.openmrs.module.kenyaemr.metadata.SecurityMetadata._Role.API_PRIVILEGES_VIEW_AND_EDIT),
-				idSet(_Privilege.KP_PROVIDER_APP, _Privilege.KP_MODULE_APP, _Privilege.PEER_CALENDAR_APP)));
-
+		    idSet(org.openmrs.module.kenyaemr.metadata.SecurityMetadata._Role.API_PRIVILEGES_VIEW_AND_EDIT),
+		    idSet(_Privilege.KP_PROVIDER_APP, _Privilege.KP_MODULE_APP, _Privilege.PEER_CALENDAR_APP)));
+		
 	}
 }
