23f4a6ae9a4e9d8364a5c9f5611386888fa39dcd
gabu90 <gabriel.mbatha@gmail.com>
2024-05-27 17:16:45 +0300

    Enhance the KVP register by adding the following columns.

---
+++ api/src/main/java/org/openmrs/module/kenyakeypop/reporting/data/converter/definition/evaluator/kp/ReceivedVLResultsDateDataEvaluator.java
@@ -0,0 +1,60 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ * <p>
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.kenyakeypop.reporting.data.converter.definition.evaluator.kp;
+
+import org.openmrs.annotation.Handler;
+import org.openmrs.module.kenyakeypop.reporting.data.converter.definition.kp.ReceivedVLResultsDateDataDefinition;
+import org.openmrs.module.reporting.data.person.EvaluatedPersonData;
+import org.openmrs.module.reporting.data.person.definition.PersonDataDefinition;
+import org.openmrs.module.reporting.data.person.evaluator.PersonDataEvaluator;
+import org.openmrs.module.reporting.evaluation.EvaluationContext;
+import org.openmrs.module.reporting.evaluation.EvaluationException;
+import org.openmrs.module.reporting.evaluation.querybuilder.SqlQueryBuilder;
+import org.openmrs.module.reporting.evaluation.service.EvaluationService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * Evaluates a PersonDataDefinition
+ */
+@Handler(supports = ReceivedVLResultsDateDataDefinition.class, order = 50)
+public class ReceivedVLResultsDateDataEvaluator implements PersonDataEvaluator {
+
+    @Autowired
+    private EvaluationService evaluationService;
+
+    public EvaluatedPersonData evaluate(PersonDataDefinition definition, EvaluationContext context)
+            throws EvaluationException {
+        EvaluatedPersonData c = new EvaluatedPersonData(definition, context);
+
+        String qry = "SELECT v.client_id AS patient_id,COALESCE(date(l.last_vl_date), date(visit_date)) AS date_of_received_vl_results\n" +
+                " FROM kenyaemr_etl.etl_clinical_visit v\n" +
+                "         LEFT JOIN (\n" +
+                "            SELECT patient_id, MAX(date(visit_date)) AS last_vl_date\n" +
+                "            FROM kenyaemr_etl.etl_laboratory_extract\n" +
+                "            WHERE lab_test IN (1305, 856)\n" +
+                "              AND COALESCE(date(date_test_requested), date(visit_date)) BETWEEN DATE(:startDate) AND DATE(:endDate)\n" +
+                "            GROUP BY patient_id\n" +
+                ") l ON v.client_id = l.patient_id\n" +
+                " WHERE DATE(v.visit_date) BETWEEN DATE(:startDate) AND DATE(:endDate);";
+
+        SqlQueryBuilder queryBuilder = new SqlQueryBuilder();
+        Date startDate = (Date) context.getParameterValue("startDate");
+        Date endDate = (Date) context.getParameterValue("endDate");
+        queryBuilder.addParameter("endDate", endDate);
+        queryBuilder.addParameter("startDate", startDate);
+        queryBuilder.append(qry);
+        Map<Integer, Object> data = evaluationService.evaluateToMap(queryBuilder, Integer.class, Object.class, context);
+        c.setData(data);
+        return c;
+    }
+}
